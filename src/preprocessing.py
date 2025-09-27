c42735b2592725cccfc8f2c5d170358ff4b28dad
Phantom611 <mayankpahade4@gmail.com>
2025-09-27 16:46:10 +0530

    commit changes

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,2 +1,37 @@
 import pandas as pd
 import os
+
+def load_and_engineer_features(AAPL_data):
+
+    df = pd.read_csv(AAPL_data)
+
+    for col in ["Open", "High", "Low", "Close"]:
+        df[col] = pd.to_numeric(df[col], errors="coerce")
+
+    df.dropna(inplace=True)
+    if "Date" in df.columns:
+        df["Date"] = pd.to_datetime(df["Date"])
+        df.set_index("Date", inplace=True)
+
+    df["Return"] = df["Close"].pct_change()
+
+    df["MA10"] = df["Close"].rolling(window=10).mean()
+    df["MA20"] = df["Close"].rolling(window=20).mean()
+
+    df["Volatility"] = df["Return"].rolling(window=10).std()
+
+    df["High_Low_spread"] = df["High"] - df["Low"]
+    df["Close_Open_change"] = df["Close"] - df["Open"]
+
+    df["Target"] = (df["Close"].shift(-1) > df["Close"]).astype(int)
+
+    df.dropna(inplace=True)
+
+    return df
+
+
+if __name__ == "__main__":
+    ##base_dir = os.path.dirname(os.path.dirname(__file__))   # go up from src/ → STOCK_PREDICTOR/
+    csv_file = os.path.join("data", "AAPL_data.csv")
+    processed_df = load_and_engineer_features(csv_file)
+    print(processed_df.head())
