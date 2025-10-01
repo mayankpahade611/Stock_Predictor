c4dd415b43553e6f6433ebe0ee5f0d17fd138931
Phantom611 <mayankpahade4@gmail.com>
2025-10-01 14:01:08 +0530

    commit changes

--- src/model_training.py
+++ src/model_training.py
@@ -15,13 +15,15 @@ def train_model(TSLA_data):
     X = df.drop(columns=["Target"])
     y = df["Target"]
 
+    # print(X.columns)
+
     X = X.select_dtypes(include=["float64", "int64"])
 
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.2, stratify=y
     )
 
-   
+    
 
     # model = RandomForestClassifier((n_estimators=100, random_state=42, class_weight="balanced")
     # model.fit(X_train, y_train)
@@ -59,10 +61,10 @@ def train_model(TSLA_data):
 
     print(model.feature_importances_)
 
-    return model, accuracy
+    return model, accuracy, X_test, y_test, y_pred
 
 if __name__ == "__main__":
 
     csv_file = os.path.join("data", "TSLA_data.csv")
-    model, acc = train_model(csv_file)
+    model, acc, X_train, y_train, y_pred = train_model(csv_file)
 
